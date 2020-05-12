6e2292870ef339c08634e807919619481a4562c5
limeng <limeng@lechebang.com>
2020-05-12 10:21:29 +0800

    feat:  add tcp resource

--- course-tcp/src/main/java/org/deanlee/course/tcp/controller/HelloController.java
+++ course-tcp/src/main/java/org/deanlee/course/tcp/controller/HelloController.java
@@ -19,22 +19,29 @@ import org.springframework.web.bind.annotation.RestController;
  * http tcp ip 链路层报文
  *
  * 三次握手
- * C---SYN: seq=j  ----->S
+ * C---SYN: seq=j  ----------------->S
  * s---SYN ACK : seq=k ack=j+1 ----->c
- * C---ACK : seq=j+1 ack=k+1 ----->S
+ * C---ACK : seq=j+1 ack=k+1 ------->S
  *
  * 数据传输
- * c-----PUH ACK: seq=j+1 ack=K+1 len=x--->s
+ * c-----PUH ACK: seq=j+1 ack=K+1 len=x--------->s
  * S-----PUH ACK: seq=k+1 ack=j+1+x len=y------->c
- * C-----ACK: seq=j+1+x+1 ack=k+1+y--------S
+ * C-----ACK: seq=j+1+x+1 ack=k+1+y------------->S
  *
  * 四次挥手
- * C-----FIN ACK: seq=j+1+x+1 ack=k+1+y--------S    C-->FIN_wait_1
- * S-----ACK: seq=k+1+y ack=j+1+x+1+1--------C      C-->Fin_wait_2   S-->close-wait
- * S-----FIN ACK: seq=K+1+y ack=j+1+x+1+1--------C  C-->Fin_wait_2   s--> last-ack
- * C-----ACK: seq=j+1+x+1+1 ack=K+1+y--------S      c-->Time_wait    c-->closed
+ * C-----FIN ACK: seq=j+1+x+1 ack=k+1+y-------->S    C-->FIN_WAIT_1
+ * S-----ACK: seq=k+1+y ack=j+1+x+1+1---------->C    C-->FIN_WAIT_2   S-->CLOSE-WAIT
+ * S-----FIN ACK: seq=K+1+y ack=j+1+x+1+1------>C    C-->FIN_WAIT_2   s-->LAST-ACK
+ * C-----ACK: seq=j+1+x+1+1 ack=K+1+y+1-------->S    c-->TIME_WAIT    c-->CLOSED
  *
  * 挥手最后client 等待一段时间2MSL 最长报文段寿命 然后再关闭对话。
+ *
+ * 三次握手/四次挥手 ack=seq+1
+ * 数据传输是 ack=seq+len  len传输数据长度
+ *
+ * 使用Wireshark 抓包处理
+ * docs文档中有抓好的包，可以做错参考
+ * 过滤条件：tcp.stream eq 496
  */
 @RestController
 public class HelloController {
--- course-tcp/src/test/java/org/deanlee/course/tcp/CourseTcpApplicationTests.java
+++ course-tcp/src/test/java/org/deanlee/course/tcp/CourseTcpApplicationTests.java
@@ -3,6 +3,8 @@ package org.deanlee.course.tcp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
+
 @SpringBootTest
 class CourseTcpApplicationTests {
 
