6ee43ae465568af5e3fd01100dadfe162af9e9c2
Dean <limeng4006@sina.com>
2020-05-16 23:03:44 +0800

    抽象工厂模式  uml closed #5

--- course-pattern/src/main/java/com/deanlee/course/pattern/factory/absfactory/AbsFactoryDemo.java
+++ course-pattern/src/main/java/com/deanlee/course/pattern/factory/absfactory/AbsFactoryDemo.java
@@ -3,6 +3,7 @@ package com.deanlee.course.pattern.factory.absfactory;
 /**
  * 工厂方法模式 demo
  * 和简单工厂模式中工厂负责生产所有产品相比，工厂方法模式将生成具体产品的任务分发给具体的产品工厂
+ * fixed 5
  */
 public class AbsFactoryDemo {
     public static void main(String[] args) {
