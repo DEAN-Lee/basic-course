df5f355861f49c7f2516f7901269a1454ea0ed4e
Dean <limeng4006@sina.com>
2020-05-22 18:36:25 +0800

    #15 责任链模式 抽象处理类

---
+++ course-pattern/src/main/java/com/deanlee/course/pattern/responsibility/Handler.java
@@ -0,0 +1,19 @@
+package com.deanlee.course.pattern.responsibility;
+
+/**
+ * 抽象处理者角色
+ */
+public abstract class Handler {
+    private Handler next;
+
+    public void setNext(Handler next) {
+        this.next = next;
+    }
+
+    public Handler getNext() {
+        return next;
+    }
+
+    //处理请求的方法
+    public abstract void handleRequest(String request);
+}
