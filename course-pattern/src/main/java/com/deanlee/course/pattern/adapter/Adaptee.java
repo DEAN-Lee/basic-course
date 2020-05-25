b5e8e21929e59520d1fbbe454525043b74be32dc
Dean <limeng4006@sina.com>
2020-05-25 23:52:13 +0800

    适配器模式  #16

---
+++ course-pattern/src/main/java/com/deanlee/course/pattern/adapter/Adaptee.java
@@ -0,0 +1,10 @@
+package com.deanlee.course.pattern.adapter;
+
+/**
+ * @author Dean github:https://github.com/DEAN-Lee
+ * @Title: Adaptee
+ * @Description: 适配者接口-已存在的接口
+ * @date 2020/5/25 23:48
+ */
+public interface Adaptee {
+}
---
+++ course-pattern/src/main/java/com/deanlee/course/pattern/adapter/Target.java
@@ -0,0 +1,10 @@
+package com.deanlee.course.pattern.adapter;
+
+/**
+ * @author Dean github:https://github.com/DEAN-Lee
+ * @Title: Target
+ * @Description: 目标实现类
+ * @date 2020/5/25 23:46
+ */
+public interface Target {
+}
