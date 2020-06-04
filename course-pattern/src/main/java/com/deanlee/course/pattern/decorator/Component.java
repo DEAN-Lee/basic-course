e82f8946fd5f5a2ff97ccd0d739b9868f6278cc0
Dean <limeng4006@sina.com>
2020-06-05 00:00:23 +0800

    #19 装饰模式

---
+++ course-pattern/src/main/java/com/deanlee/course/pattern/decorator/Component.java
@@ -0,0 +1,11 @@
+package com.deanlee.course.pattern.decorator;
+
+/**
+ * @author Dean github:https://github.com/DEAN-Lee
+ * @Title: Component
+ * @Description: 抽象构建角色：定义一个抽象接口以规范准备接收附加责任的对象。
+ * @date 2020/6/4 23:55
+ */
+public interface Component {
+    void operation();
+}
