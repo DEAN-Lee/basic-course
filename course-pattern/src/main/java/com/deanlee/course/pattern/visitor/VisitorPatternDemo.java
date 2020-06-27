3b5dcd4486d7fbdc7b1a0f8ced646d499e6938b7
Dean <limeng4006@sina.com>
2020-06-27 22:54:36 +0800

    #21 访问者模式-visitor pattern  主要类

---
+++ course-pattern/src/main/java/com/deanlee/course/pattern/visitor/VisitorPatternDemo.java
@@ -0,0 +1,13 @@
+package com.deanlee.course.pattern.visitor;
+
+/**
+ * @author Dean github:https://github.com/DEAN-Lee
+ * @Title: VisitorPatternDemo
+ * @Description: 访问者类Demo
+ * @date 2020/6/26 23:20
+ */
+public class VisitorPatternDemo {
+    public static void main(String[] args) {
+
+    }
+}
