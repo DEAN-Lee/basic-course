5bb67d1c6423178c266c584b15cf41055e5a1fa2
Dean <limeng4006@sina.com>
2020-06-30 22:36:10 +0800

    #22 桥接模式-bridge pattern demo

---
+++ course-pattern/src/main/java/com/deanlee/course/pattern/bridge/BridgePatternDemo.java
@@ -0,0 +1,13 @@
+package com.deanlee.course.pattern.bridge;
+
+/**
+ * @author Dean github:https://github.com/DEAN-Lee
+ * @Title: BridgePatternDemo
+ * @Description: 桥接模式--示例
+ * @date 2020/6/30 22:34
+ */
+public class BridgePatternDemo {
+    public static void main(String[] args) {
+
+    }
+}
