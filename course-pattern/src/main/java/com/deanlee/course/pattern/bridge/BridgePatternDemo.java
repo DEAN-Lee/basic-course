27957da54eb1e7dd3b42ddae8d829664b2504808
Dean <limeng4006@sina.com>
2020-07-04 22:45:45 +0800

    #22 桥接模式-bridge pattern 主要角色 demo

--- course-pattern/src/main/java/com/deanlee/course/pattern/bridge/BridgePatternDemo.java
+++ course-pattern/src/main/java/com/deanlee/course/pattern/bridge/BridgePatternDemo.java
@@ -8,6 +8,8 @@ package com.deanlee.course.pattern.bridge;
  */
 public class BridgePatternDemo {
     public static void main(String[] args) {
-
+        Implementor imple=new ConcreteImplementorA();
+        Abstraction abs=new RefinedAbstraction(imple);
+        abs.Operation();
     }
 }
