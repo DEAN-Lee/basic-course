b49fd94641192c42096e07ab30a86f02668f8040
Dean <limeng4006@sina.com>
2020-07-07 22:24:42 +0800

    #23 过滤器模式-filter pattern 主要角色

---
+++ course-pattern/src/main/java/com/deanlee/course/pattern/filter/filterPatternDemo.java
@@ -0,0 +1,13 @@
+package com.deanlee.course.pattern.filter;
+
+/**
+ * @author Dean github:https://github.com/DEAN-Lee
+ * @Title: filterPatternDemo
+ * @Description: 过滤器模式demo
+ * @date 2020/7/7 22:23
+ */
+public class filterPatternDemo {
+    public static void main(String[] args) {
+
+    }
+}
