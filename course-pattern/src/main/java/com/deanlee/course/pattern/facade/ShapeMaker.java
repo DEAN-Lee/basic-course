75b41bf23bf9edfa02108432e024645fec553795
Dean <limeng4006@sina.com>
2020-07-12 23:10:05 +0800

    #24 外观模式-Facade pattern 主要角色

---
+++ course-pattern/src/main/java/com/deanlee/course/pattern/facade/ShapeMaker.java
@@ -0,0 +1,10 @@
+package com.deanlee.course.pattern.facade;
+
+/**
+ * @author Dean github:https://github.com/DEAN-Lee
+ * @Title: ShapeMaker
+ * @Description: 外观类.封装接口
+ * @date 2020/7/12 23:09
+ */
+public class ShapeMaker {
+}
