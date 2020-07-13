b94a9ae04750f4dd9a970b73c49b7d7b570e2ee6
Dean <limeng4006@sina.com>
2020-07-13 18:13:03 +0800

    #24 外观模式-Facade pattern 主要角色

--- course-pattern/src/main/java/com/deanlee/course/pattern/facade/FacadePatternDemo.java
+++ course-pattern/src/main/java/com/deanlee/course/pattern/facade/FacadePatternDemo.java
@@ -5,6 +5,10 @@ package com.deanlee.course.pattern.facade;
  */
 public class FacadePatternDemo {
     public static void main(String[] args) {
+        ShapeMaker shapeMaker = new ShapeMaker();
 
+        shapeMaker.drawCircle();
+        shapeMaker.drawRectangle();
+        shapeMaker.drawSquare();
     }
 }
--- course-pattern/src/main/java/com/deanlee/course/pattern/facade/ShapeMaker.java
+++ course-pattern/src/main/java/com/deanlee/course/pattern/facade/ShapeMaker.java
@@ -7,4 +7,25 @@ package com.deanlee.course.pattern.facade;
  * @date 2020/7/12 23:09
  */
 public class ShapeMaker {
+    private Shape circle;
+    private Shape rectangle;
+    private Shape square;
+
+    public ShapeMaker() {
+        circle = new CircleShape();
+        rectangle = new RectangleShape();
+        square = new SquareShape();
+    }
+
+    public void drawCircle() {
+        circle.draw();
+    }
+
+    public void drawRectangle() {
+        rectangle.draw();
+    }
+
+    public void drawSquare() {
+        square.draw();
+    }
 }
