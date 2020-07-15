860e53df7806bdb38f406506c90b9d2b153ac423
Dean <limeng4006@sina.com>
2020-07-15 23:23:46 +0800

    组合模式-Composite pattern

---
+++ course-pattern/src/main/java/com/deanlee/course/pattern/composite/CompositePatternDemo.java
@@ -0,0 +1,13 @@
+package com.deanlee.course.pattern.composite;
+
+/**
+ * @author Dean github:https://github.com/DEAN-Lee
+ * @Title: CompositePatternDemp
+ * @Description: 组合模式
+ * @date 2020/7/15 23:22
+ */
+public class CompositePatternDemo {
+    public static void main(String[] args) {
+
+    }
+}
