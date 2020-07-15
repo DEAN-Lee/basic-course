843b8e0b218e4bc799d78e157e1b73417f9e82a0
Dean <limeng4006@sina.com>
2020-07-15 23:17:13 +0800

    closed #24 外观模式-Facade pattern uml

--- course-pattern/src/main/java/com/deanlee/course/pattern/facade/FacadePatternDemo.java
+++ course-pattern/src/main/java/com/deanlee/course/pattern/facade/FacadePatternDemo.java
@@ -6,7 +6,6 @@ package com.deanlee.course.pattern.facade;
 public class FacadePatternDemo {
     public static void main(String[] args) {
         ShapeMaker shapeMaker = new ShapeMaker();
-
         shapeMaker.drawCircle();
         shapeMaker.drawRectangle();
         shapeMaker.drawSquare();
