4e9b30f4dbd21d5a1864f543dd2ce113dc04acf7
Dean <limeng4006@sina.com>
2020-07-23 23:12:10 +0800

    #26  命令模式-Command Pattern 主要类

---
+++ course-pattern/src/main/java/com/deanlee/course/pattern/command/Command.java
@@ -0,0 +1,12 @@
+package com.deanlee.course.pattern.command;
+
+/**
+ * @author Dean github:https://github.com/DEAN-Lee
+ * @Title: Command
+ * @Description: 抽象命令类（Command）角色:声明执行命令的接口，拥有执行命令的抽象方法 execute()。
+ * @date 2020/7/23 23:11
+ */
+public interface Command {
+    public abstract void execute();
+
+}
