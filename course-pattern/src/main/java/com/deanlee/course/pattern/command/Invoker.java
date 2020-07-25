f5869c910fc6a930c2c0d7231a54306ed78248e3
Dean <limeng4006@sina.com>
2020-07-25 23:11:48 +0800

    #26  命令模式-Command Pattern 主要类

---
+++ course-pattern/src/main/java/com/deanlee/course/pattern/command/Invoker.java
@@ -0,0 +1,24 @@
+package com.deanlee.course.pattern.command;
+
+/**
+ * @author Dean github:https://github.com/DEAN-Lee
+ * @Title: Invoker
+ * @Description: 调用者/请求者（Invoker）角色：是请求的发送者，它通常拥有很多的命令对象，并通过访问命令对象来执行相关请求，它不直接访问接收者。
+ * @date 2020/7/25 23:11
+ */
+public class Invoker {
+    private Command command;
+
+    public Invoker(Command command) {
+        this.command = command;
+    }
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    public void call() {
+        System.out.println("调用者执行命令command...");
+        command.execute();
+    }
+}
