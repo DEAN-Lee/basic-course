ebc17caf638ac5aabcc1e79cd8a9f94dd65420cd
Dean <limeng4006@sina.com>
2020-07-26 22:03:04 +0800

    #26  命令模式-Command Pattern uml

--- course-pattern/src/main/java/com/deanlee/course/pattern/command/CommandPatternDemo.java
+++ course-pattern/src/main/java/com/deanlee/course/pattern/command/CommandPatternDemo.java
@@ -8,6 +8,9 @@ package com.deanlee.course.pattern.command;
  */
 public class CommandPatternDemo {
     public static void main(String[] args) {
-
+        Command cmd = new ConcreteCommand();
+        Invoker ir = new Invoker(cmd);
+        System.out.println("客户访问调用者的call()方法...");
+        ir.call();
     }
 }
