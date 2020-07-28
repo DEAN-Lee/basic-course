93b18dbcc4b0a15a99fe2a738b3e8c3ded30c220
Dean <limeng4006@sina.com>
2020-07-28 23:08:50 +0800

    #27 中介者模式-Mediator Pattern

---
+++ course-pattern/src/main/java/com/deanlee/course/pattern/mediator/MediatorPatternDemo.java
@@ -0,0 +1,13 @@
+package com.deanlee.course.pattern.mediator;
+
+/**
+ * @author Dean github:https://github.com/DEAN-Lee
+ * @Title: MediatorPatternDemo
+ * @Description: 中介者模式
+ * @date 2020/7/28 23:06
+ */
+public class MediatorPatternDemo {
+    public static void main(String[] args) {
+
+    }
+}
