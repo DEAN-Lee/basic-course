34a3cce6038bf1c38b8f4250c3a63f9a40113dfb
Dean <limeng4006@sina.com>
2020-07-31 23:26:36 +0800

    #27 中介者模式-Mediator Pattern demo

--- course-pattern/src/main/java/com/deanlee/course/pattern/mediator/MediatorPatternDemo.java
+++ course-pattern/src/main/java/com/deanlee/course/pattern/mediator/MediatorPatternDemo.java
@@ -8,6 +8,14 @@ package com.deanlee.course.pattern.mediator;
  */
 public class MediatorPatternDemo {
     public static void main(String[] args) {
-
+        Mediator md = new ConcreteMediator();
+        Colleague c1, c2;
+        c1 = new ConcreteColleagueOne();
+        c2 = new ConcreteColleagueTwo();
+        md.register(c1);
+        md.register(c2);
+        c1.send();
+        System.out.println("-------------");
+        c2.send();
     }
 }
