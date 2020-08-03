eea6f181ee99b35f435ebc13f7bcc20f8b646328
Dean <limeng4006@sina.com>
2020-08-03 22:17:22 +0800

    #28 备忘录模式-Memento Pattern  又称快照模式

---
+++ course-pattern/src/main/java/com/deanlee/course/pattern/memento/MementoPatternDemo.java
@@ -0,0 +1,13 @@
+package com.deanlee.course.pattern.memento;
+
+/**
+ * @author Dean github:https://github.com/DEAN-Lee
+ * @Title: MementoPatternDemo
+ * @Description: 备忘录模式
+ * @date 2020/8/3 22:16
+ */
+public class MementoPatternDemo {
+    public static void main(String[] args) {
+
+    }
+}
