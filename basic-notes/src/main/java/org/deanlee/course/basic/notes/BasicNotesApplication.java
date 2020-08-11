ac4093dcbb33b042f861d31f583322e4d9dbe15b
Dean <limeng4006@sina.com>
2020-08-11 18:11:59 +0800

    log 总结

---
+++ basic-notes/src/main/java/org/deanlee/course/basic/notes/BasicNotesApplication.java
@@ -0,0 +1,13 @@
+package org.deanlee.course.basic.notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicNotesApplication.class, args);
+    }
+
+}
