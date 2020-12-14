5497c43ef032a8fbe16a4e53e305beec8535da25
Dean <limeng4006@sina.com>
2020-12-14 20:10:48 +0800

    设计模式修改

---
+++ course-algorithm/src/main/java/org/deanlee/course/algorithm/binarysearch/TestFinal.java
@@ -0,0 +1,10 @@
+package org.deanlee.course.algorithm.binarysearch;
+
+import java.util.Arrays;
+
+public class TestFinal {
+    public static void main(String[] args) {
+        final Boolean d = new Boolean(true);
+        System.out.println("args = " + d);
+    }
+}
