92bf17d90126f93a09aa37bf72fe556e75a1eb50
Dean <limeng4006@sina.com>
2021-03-08 15:04:17 +0800

    java NIO中Buffer中ByteBuffer

--- course-io/src/main/java/org/deanlee/course/networkio/noblockingio/BufferDemo.java
+++ course-io/src/main/java/org/deanlee/course/networkio/noblockingio/BufferDemo.java
@@ -13,7 +13,9 @@ public class BufferDemo {
      * 二进制 测试
      */
     public static void byteBufferTest() {
+        //初始
         ByteBuffer byteBuffer = ByteBuffer.allocate(16);
+        //赋值
         byteBuffer.put((byte) 1);
         byteBuffer.put((byte) 2);
         byteBuffer.put((byte) 2);
@@ -28,16 +30,17 @@ public class BufferDemo {
         byteBuffer.put((byte) 4);
         byteBuffer.put((byte) 4);
         byteBuffer.put((byte) 6);
-
+        //打印参数参数
         System.out.println("before------>limit "+byteBuffer.limit());
         System.out.println("before------>position"+byteBuffer.position());
         System.out.println("before------>capacity"+byteBuffer.capacity());
-
+        //切换模式读模式
         byteBuffer.flip();
-
+        //打印参数参数
         System.out.println("after------>limit"+byteBuffer.limit());
         System.out.println("after------>position"+byteBuffer.position());
         System.out.println("after------>capacity"+byteBuffer.capacity());
+        //输出内容
         int i = 0;
         while (i < byteBuffer.limit()) {
             System.out.println(byteBuffer.get(i));
