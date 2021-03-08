ba3b68e7ad949aa5202f9bdf8dc1b1c88e7a5333
Dean <limeng4006@sina.com>
2021-03-08 16:19:42 +0800

    java NIO中Buffer中ByteBuffer

--- course-io/src/main/java/org/deanlee/course/networkio/noblockingio/BufferDemo.java
+++ course-io/src/main/java/org/deanlee/course/networkio/noblockingio/BufferDemo.java
@@ -18,35 +18,45 @@ public class BufferDemo {
         //赋值
         byteBuffer.put((byte) 1);
         byteBuffer.put((byte) 2);
-        byteBuffer.put((byte) 2);
-        byteBuffer.put((byte) 2);
-        byteBuffer.put((byte) 2);
-        byteBuffer.put((byte) 2);
+        byteBuffer.put((byte) 3);
         byteBuffer.put((byte) 4);
-        byteBuffer.put((byte) 4);
-        byteBuffer.put((byte) 4);
-        byteBuffer.put((byte) 4);
-        byteBuffer.put((byte) 4);
-        byteBuffer.put((byte) 4);
-        byteBuffer.put((byte) 4);
+        byteBuffer.put((byte) 5);
         byteBuffer.put((byte) 6);
-        //打印参数参数
-        System.out.println("before------>limit "+byteBuffer.limit());
-        System.out.println("before------>position"+byteBuffer.position());
-        System.out.println("before------>capacity"+byteBuffer.capacity());
+        byteBuffer.put((byte) 7);
+        byteBuffer.put((byte) 8);
+        byteBuffer.put((byte) 9);
+        byteBuffer.put((byte) 10);
+        byteBuffer.put((byte) 11);
+        byteBuffer.put((byte) 12);
+        byteBuffer.put((byte) 13);
+        byteBuffer.put((byte) 14);
+        //打印参数值
+        System.out.println("before------>limit " + byteBuffer.limit());
+        System.out.println("before------>position" + byteBuffer.position());
+        System.out.println("before------>capacity" + byteBuffer.capacity());
         //切换模式读模式
         byteBuffer.flip();
-        //打印参数参数
-        System.out.println("after------>limit"+byteBuffer.limit());
-        System.out.println("after------>position"+byteBuffer.position());
-        System.out.println("after------>capacity"+byteBuffer.capacity());
-        //输出内容
-        int i = 0;
-        while (i < byteBuffer.limit()) {
-            System.out.println(byteBuffer.get(i));
-            i++;
+        //输出参数值
+        System.out.println("after------>limit" + byteBuffer.limit());
+        System.out.println("after------>position" + byteBuffer.position());
+        System.out.println("after------>capacity" + byteBuffer.capacity());
+        //输出2buffer数量
+        for (int i = 0; i < 2; i++) {
+            System.out.println(byteBuffer.get());
         }
 
+        //输出3buffer数量 后 参数值
+        System.out.println("after1------>limit" + byteBuffer.limit());
+        System.out.println("after1------>position" + byteBuffer.position());
+        System.out.println("after1------>capacity" + byteBuffer.capacity());
+        //输出内容
+        for (int i = 0; i < 4; i++) {
+            System.out.println(byteBuffer.get());
+        }
+        // 输出4buffer数量 后 参数值
+        System.out.println("after2------>limit" + byteBuffer.limit());
+        System.out.println("after2------>position" + byteBuffer.position());
+        System.out.println("after2------>capacity" + byteBuffer.capacity());
     }
 
     public static void main(String[] args) {
