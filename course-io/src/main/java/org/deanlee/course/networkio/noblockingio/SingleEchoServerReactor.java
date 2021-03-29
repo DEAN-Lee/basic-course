e58fcaa27358042676a7fe8553b76df461a1dfb9
Dean <limeng4006@sina.com>
2021-03-29 18:03:32 +0800

    Reactor反应器模式.md

--- course-io/src/main/java/org/deanlee/course/networkio/noblockingio/SingleEchoServerReactor.java
+++ course-io/src/main/java/org/deanlee/course/networkio/noblockingio/SingleEchoServerReactor.java
@@ -4,33 +4,26 @@ import java.io.IOException;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 import java.util.Set;
 
-/**
- * 单线程 reactor模式
- */
-public class SingleReactorServer implements Runnable {
-
+public class SingleEchoServerReactor implements Runnable {
     Selector selector;
     ServerSocketChannel serverSocket;
 
     SingleEchoServerReactor() throws IOException {
-//....省略：打开选择器、serverSocket连接监听通道
-//注册serverSocket的accept事件
-        SelectionKey sk =
-                serverSocket.register(selector, SelectionKey.OP_ACCEPT);
-//将新连接处理器作为附件，绑定到sk选择键
-        sk.attach(new AcceptorHandler());
+//...获取选择器、开启serverSocket服务监听通道
+//...绑定AcceptorHandler新连接处理器到selectKey
     }
 
+    //轮询和分发事件
     public void run() {
-//选择器轮询
         try {
             while (!Thread.interrupted()) {
                 selector.select();
-                Set selected = selector.selectedKeys();
-                Iterator it = selected.iterator();
+                Set<SelectionKey> selected = selector.selectedKeys();
+                Iterator<SelectionKey> it = selected.iterator();
                 while (it.hasNext()) {
 //反应器负责dispatch收到的事件
                     SelectionKey sk = it.next();
@@ -43,20 +36,28 @@ public class SingleReactorServer implements Runnable {
         }
     }
 
-    //反应器的分发方法
-    void dispatch(SelectionKey k) {
-        Runnable handler = (Runnable) (k.attachment());
-//调用之前绑定到选择键的handler处理器对象
+    void dispatch(SelectionKey sk) {
+        Runnable handler = (Runnable) sk.attachment();
+//调用之前attach绑定到选择键的handler处理器对象
         if (handler != null) {
             handler.run();
         }
     }
 
-    // 新连接处理器
+    // Handler:新连接处理器
     class AcceptorHandler implements Runnable {
         public void run() {
-//接受新连接
-//需要为新连接，创建一个输入输出的handler处理器
+            try {
+                SocketChannel channel = serverSocket.accept();
+                if (channel != null)
+                    new EchoHandler(selector, channel);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Thread(new SingleEchoServerReactor()).start();
     }
 }
