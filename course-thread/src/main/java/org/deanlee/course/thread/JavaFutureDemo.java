3b8db835c58f061a4a86e51b0a8c3137e1053050
Dean <limeng4006@sina.com>
2021-04-06 18:17:13 +0800

    #46 并发线程基础-futureTask

---
+++ course-thread/src/main/java/org/deanlee/course/thread/JavaFutureDemo.java
@@ -0,0 +1,68 @@
+package org.deanlee.course.thread;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+/**
+ * future task demo
+ * @author dean_lee
+ */
+public class JavaFutureDemo {
+    static class AThread implements Callable<Boolean>{
+        /**
+         * Computes a result, or throws an exception if unable to do so.
+         *
+         * @return computed result
+         * @throws Exception if unable to compute a result
+         */
+        @Override
+        public Boolean call() throws Exception {
+            System.out.println(Thread.currentThread().getName() + "AThread start");
+            Thread.sleep(4000);
+            System.out.println(Thread.currentThread().getName() + "BThread end");
+            return true;
+        }
+    }
+
+    static class BThread implements Callable<Boolean>{
+        /**
+         * Computes a result, or throws an exception if unable to do so.
+         *
+         * @return computed result
+         * @throws Exception if unable to compute a result
+         */
+        @Override
+        public Boolean call() throws Exception {
+            System.out.println(Thread.currentThread().getName() + "BThread start");
+            Thread.sleep(2000);
+            System.out.println(Thread.currentThread().getName() + "BThread end");
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        long times = System.currentTimeMillis();
+        System.out.println(Thread.currentThread().getName() + "main start " + times);
+
+        FutureTask<Boolean> aTask = new FutureTask(new AThread());
+        Thread aThread = new Thread(aTask, "** A-Thread");
+
+        FutureTask<Boolean> bTask = new FutureTask(new BThread());
+        Thread bThread = new Thread(bTask, "** b-Thread");
+        aThread.start();
+        bThread.start();
+
+        try {
+            boolean  warterOk = aTask.get();
+            boolean  cupOk = bTask.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + "main done "+ ((System.currentTimeMillis()-times)/1000));
+
+
+    }
+}
