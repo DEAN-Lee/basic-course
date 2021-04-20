50fecea6f8a378cf11760da1df7a2451cb76b0f9
Dean <limeng4006@sina.com>
2021-04-20 20:34:21 +0800

    #47 netty 基础-handler处理

---
+++ course-thread/src/main/java/org/deanlee/course/thread/netty/NettyDiscardHandler.java
@@ -0,0 +1,23 @@
+package org.deanlee.course.thread.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+public class NettyDiscardHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            System.out.println("收到消息,丢弃如下:");
+            while (in.isReadable()) {
+                System.out.print((char) in.readByte());
+            }
+            System.out.println();
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+}
